223df5c1a301b38ad863481dd83f0804ed1cc85c
Shivam Sharma <123826841+Shivam-sharmag@users.noreply.github.com>
2023-02-18 10:05:58 +0530

    Update youtube_bot.py

--- youtube_bot.py
+++ youtube_bot.py
@@ -3,6 +3,6 @@ import chromedriver_binary  # Adds chromedriver binary to path
 import time 
 driver = webdriver.Chrome()
 while True:
-    driver.get("INPUT YOUT VIDEO URL HERE")
+    driver.get("https://www.youtube.com/watch?v=vAa951sMoFg")
     time.sleep(5)
 
